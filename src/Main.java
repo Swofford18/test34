6ea799f9a34c194276ab2eac58c133c317e524a5
Andrey <arkh.andr3w@gmail.com>
2020-09-26 21:15:00 +0300

    delete oleg

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("privet + zdarova");
-        System.out.println("privet");
-
-        System.out.println("oleg");
     }
 }
